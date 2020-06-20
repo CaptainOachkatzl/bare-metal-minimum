51e1edecbabb9a569d9f5e9b9008c7f458409538
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-20 15:58:36 +0200

    removed debugging code

--- stage1.asm
+++ stage1.asm
@@ -74,33 +74,6 @@ read_sectors_16:
     pop ax
     retn
 
-; print a number in hex
-; IN
-;   bx: the number
-; CLOBBER
-;   al, cx
-real_mode_print_hex:
-    mov cx, 4
-.lp:
-    mov al, bh
-    shr al, 4
-
-    cmp al, 0xA
-    jb .below_0xA
-
-    add al, 'A' - 0xA - '0'
-.below_0xA:
-    add al, '0'
-
-    call real_mode_print_char
-
-    shl bx, 4
-    loop .lp
-
-    call real_mode_new_line
-
-    ret
-
 ; print string
 ; IN
 ;   si : pointer to string
