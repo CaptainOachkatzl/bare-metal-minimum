df74970a9ae9ceeba25609a47c66f16566c6f390
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-06-21 12:08:02 +0200

    added GDT tables for protected mode

--- stage1.asm
+++ stage1.asm
@@ -118,6 +118,34 @@ stage1_loading_string:
 error_loading_string:
     db 'Error while loading stage 2 from disk!', 0
 
+gdt32info:
+    dw gdt32_end - gdt32 - 1    ; size - decrement by 1 because size = 0 is not allowed 
+                                ; and this way size can be the full 65536 bytes
+    dw gdt32                    ; offset/start of table
+
+gdt32:
+    dq 0   ; first segment descriptor of GDT -> unused
+codedesc:
+    ; code segment descriptor, Executable bit Ex must be set, RW set to make segment readable
+    ; with Gr set to 1 the granularity is set to 4KiB, combined with limit = 0xFFFFFF,  64GB of memory is addressable
+    ; because of 32 bit address registers -> 4GB addressable
+    ; L has to be zero if Sz is set because combination is reserved and will throw an exception otherwise
+    dw 0xFFFF       ; limit/segment 0-15
+    dw 0x0000       ; base addr 0-15
+    db 0x00         ; base addr 16-23
+    db 0b10011010   ; access byte -> in order: Pr - Privi2 - Privi1 - S - Ex - DC - RW - Ac
+    db 0b11001111   ; first 4 bits = flags, Gr - Sz - L - 0; last 4 bits = limit/segment 16-19
+    db 0x00         ; base addr 24-31
+datadesc:
+    ; data segment descriptor, Executable bit Ex must not be set, RW set to make segment writeable
+    dw 0xFFFF       ; limit/segment 0-15
+    dw 0x0000       ; base addr 0-15
+    db 0x00         ; base addr 16-23
+    db 0b10010010   ; access byte -> in order: Pr - Privi2 - Privi1 - S - Ex - DC - RW - Ac
+    db 0b11001111   ; first 4 bits = flags, Gr - Sz - L - 0; last 4 bits = limit/segment 16-19
+    db 0x00         ; base addr 24-31
+gdt32_end:
+
 ; boot signature
     TIMES 510-($-$$) db 0
     dw 0xAA55
